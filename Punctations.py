3415b880b9f91f90fbfee600d8606fbb90a229ef
Ahmad Sheikh <luqasheikhq123@gmail.com>
2024-09-26 21:35:16 +0500

    delete

--- Punctations.py
+++ Punctations.py
@@ -1,7 +1,1 @@
-punctations = """!()-[]{};:'"\,<>./?@#$%^&*_~"""
-string = input("Enter a string: ")
-string = string.lower()
-for char in string:
-    if char in punctations:
-        string = string.replace(char, " " )
-print(string)
+
